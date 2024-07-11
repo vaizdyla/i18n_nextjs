ed4c1aa540f33580b938170b761eb68240a780bb
Vaizdyla <m.smiginas@gmail.com>
2024-07-11 23:42:09 +0300

    Pradinis next-intl nustatymas

---
+++ middleware.ts
@@ -0,0 +1,14 @@
+import createMiddleware from 'next-intl/middleware';
+
+export default createMiddleware({
+  // A list of all locales that are supported
+  locales: ['lt', 'en', 'ru'],
+
+  // Used when no locale matches
+  defaultLocale: 'lt',
+});
+
+export const config = {
+  // Match only internationalized pathnames
+  matcher: ['/', '/(lt|en|ru)/:path*'],
+};
